54f4697f8c17abb5c6d0acc23de942e9e83cccc1
Melkory <carloseduardo.dziombra@hotmail.com>
2025-05-05 15:09:09 -0300

    Método findAllPaged

--- src/main/java/com/devsuperior/bds02/controllers/CityController.java
+++ src/main/java/com/devsuperior/bds02/controllers/CityController.java
@@ -2,20 +2,26 @@ package com.devsuperior.bds02.controllers;
 
 import com.devsuperior.bds02.dto.CityDTO;
 import com.devsuperior.bds02.services.CityService;
+import jdk.jfr.Category;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping(value = "/cities")
 public class CityController {
 
     @Autowired
     private CityService service;
 
     @GetMapping
-    public List<CityDTO> findAll () {
-        return null;
+    public ResponseEntity<Page<CityDTO>> findAll ( Pageable pageable ) {
+        Page<CityDTO> result = service.findAllPaged(pageable);
+        return ResponseEntity.ok(result);
     }
 
     @PostMapping
--- src/main/java/com/devsuperior/bds02/services/CityService.java
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -1,12 +1,13 @@
 package com.devsuperior.bds02.services;
 
 import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
 import com.devsuperior.bds02.repository.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CityService {
@@ -14,8 +15,10 @@ public class CityService {
     @Autowired
     private CityRepository repository;
 
-    public List<CityDTO> findAll ( Pageable pageable ) {
-        return null;
+    @Transactional(readOnly = true)
+    public Page<CityDTO> findAllPaged ( Pageable pageable ) {
+        Page<City> result = repository.findAll(pageable);
+        return result.map(x -> new CityDTO(x));
     }
 
     public CityDTO insert (CityDTO dto) {
