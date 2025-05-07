f2f825b30a16e3146ff2c02f8e31f6bb4e17c392
Melkory <carloseduardo.dziombra@hotmail.com>
2025-05-07 20:22:05 -0300

    EventCity update

--- src/main/java/com/devsuperior/bds02/controllers/EventController.java
+++ src/main/java/com/devsuperior/bds02/controllers/EventController.java
@@ -3,18 +3,20 @@ package com.devsuperior.bds02.controllers;
 import com.devsuperior.bds02.dto.EventDTO;
 import com.devsuperior.bds02.services.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(value = "/events")
 public class EventController {
 
     @Autowired
     private EventService service;
 
-    @PutMapping
-    public EventDTO update (EventDTO dto, Long id) {
-        return null;
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<EventDTO> update ( @RequestBody EventDTO dto, @PathVariable Long id) {
+        dto = service.update(dto, id);
+        return ResponseEntity.ok().body(dto);
     }
 
 }
--- src/main/java/com/devsuperior/bds02/services/EventService.java
+++ src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -1,9 +1,13 @@
 package com.devsuperior.bds02.services;
 
 import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.entities.Event;
+import com.devsuperior.bds02.repository.CityRepository;
 import com.devsuperior.bds02.repository.EventRepository;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PutMapping;
 
 @Service
@@ -12,8 +16,21 @@ public class EventService {
     @Autowired
     private EventRepository repository;
 
+    @Autowired
+    private CityRepository cityRepository;
+
+    @Transactional
     public EventDTO update (EventDTO dto, Long id) {
-        return null;
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException("Recurso não encontrado!");
+        }
+        Event entity = repository.getReferenceById(id);
+        entity.setName(dto.getName());
+        entity.setDate(dto.getDate());
+        entity.setUrl(dto.getUrl());
+        entity.setCity(cityRepository.getReferenceById(dto.getCityId()));
+        entity = repository.save(entity);
+        return new EventDTO(entity);
     }
 
 }
